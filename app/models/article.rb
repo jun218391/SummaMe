e430fda0c97a3578073f5d7966c795aa74a09757
jun218391 <gongzezhun@gmail.com>
2023-03-27 06:27:27 +0000

    [modified]管理者　レイアウト

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,21 +8,21 @@ class Article < ApplicationRecord
   scope :published, -> {where(is_published_flag: true)}
   scope :unpublished, -> {where(is_published_flag: false)}
  
-  # 画像
-  def get_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-      profile_image.variant(resize_to_limit: [width, height]).processed
-  end
+  # # 画像
+  # def get_image(width, height)
+  #   unless profile_image.attached?
+  #     file_path = Rails.root.join('app/assets/images/no_image.jpg')
+  #     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #     profile_image.variant(resize_to_limit: [width, height]).processed
+  # end
   
   # ログインユーザーが記事にいいねしているかを判定
   def favorited_by?(customer)
     favorites.exists?(customer_id: customer.id)
   end
   
-  # 投稿検索（部分検索）
+  # 投稿タイトル検索（部分検索）
   def self.looks(word)
     @article = Article.where("title LIKE?","%#{word}%")
   end
