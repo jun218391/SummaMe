b93d0f0f48de71a472537109464c5e0a9f1cf4eb
jun218391 <gongzezhun@gmail.com>
2023-03-28 08:32:18 +0000

    [update]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ Admin.create!(
     password: 'SummaMe',
 )
 
+# テストデータ
 customers = Customer.create!(
   [
     {email: 'olivia@test.com', name: 'Olivia', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
@@ -34,15 +35,31 @@ Article.create!(
     is_published_flag: true},
     
     {title: 'ビジネスをより効果的にする最善の方法',
-    content: '大人気のカフェです。', 
-    todo: '大人気のカフェです。',
+    content: '仕事の全体像と目標を抑える。全体像から計画を立てタスクの優先順位を
+    付ける。', 
+    todo: '',
     book_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), 
     customer_id: customers[1].id,
     is_published_flag: true },
     
     {title: 'ShoreditchBar',
-    content: '大人気のカフェです。', 
-    todo: '大人気のカフェです。',
+    content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+    Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
+    nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, 
+    pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, 
+    fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, 
+    venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. 
+    Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, 
+    consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
+    tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+    Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. 
+    Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, 
+    sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, 
+    luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus.
+    Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. 
+    ]Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. 
+    Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,', 
+    todo: 'vitae, eleifend ac, enim. Aliquam',
     book_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), 
     customer_id: customers[2].id,
     is_published_flag: true}
