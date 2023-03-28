8cda50ed17d94c8ee7a356e705e84448c729dc70
jun218391 <gongzezhun@gmail.com>
2023-03-28 12:47:19 +0000

    [modified]

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -21,6 +21,12 @@ class Admin::ArticlesController < ApplicationController
     end
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  
   private
   
   def article_params
